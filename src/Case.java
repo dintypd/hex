fc4481c95a25d8080ea5996a05c07e63f9d5d370
BenjouLbambou <E124598H@I012V7pc04.ensinfo.sciences.univ-nantes.prive>
2015-10-14 12:18:16 +0200

    case

---
+++ src/Case.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+
+public class Case {
+	private int x_;
+	private int y_;
+	private int couleur_;
+	private Case representant_;
+	private ArrayList<Case> fils_;
+}
---
+++ src/Pion.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+
+public class Pion {
+	//private Case case_;
+	private int couleur_;
+	private Pion representant_;
+	private ArrayList<Pion> fils_;
+
+}
